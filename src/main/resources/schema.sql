12df50e82b80c70bf61d11d781a8db8ac4e51a4a
Marc Angers <50119813+MarcAngers@users.noreply.github.com>
2022-03-21 17:39:09 -0400

    Added Database?

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE user
+(
+username varchar(50) NOT NULL,
+name varchar(50) NOT NULL,
+password varchar(50) NOT NULL,
+PRIMARY KEY (username)
+)
+CREATE TABLE customer
+(
+username varchar(50) NOT NULL,
+accessExpiration Date,
+FOREIGN KEY (username) REFERENCES user(username)
+)
+CREATE TABLE admin
+(
+username varchar(50) NOT NULL,
+FOREIGN KEY (username) REFERENCES user(username)
+)
