381e0005a2cd7c87267b23fc57fae0728871411c
Marc Angers <50119813+MarcAngers@users.noreply.github.com>
2022-03-23 13:06:39 -0400

    feex part2

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,6 +3,7 @@ CREATE TABLE users
 username varchar(50) NOT NULL,
 name varchar(50) NOT NULL,
 password varchar(50) NOT NULL,
+subscription varchar(50) NOT NULL,
 access_expiration Date,
 dtype varchar(50),
 PRIMARY KEY (username)
